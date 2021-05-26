9b83bc01340aba913801ec412813bfa9da1efbf0
Hadif Hatta <45189724+hdfhtt@users.noreply.github.com>
2021-05-26 14:17:03 +0800

    Improve code quality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Media;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
+using System.Windows.Controls;
 
 namespace ic_req_helper
 {
@@ -20,6 +17,13 @@ namespace ic_req_helper
         {
             InitializeComponent();
             CenterWindowOnScreen();
+
+            MenuItem[] menuItems = { menuLocate0, menuLocate1, menuLocate2, menuLocate3, menuLocate4, menuLocate5, menuReset, menuExit }; 
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                menuItems[i].Click += menu_Click;
+            }
         }
 
         private void CenterWindowOnScreen()
@@ -51,6 +55,7 @@ namespace ic_req_helper
             Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
 
             string[] fileNames = { "appfilter.xml", "appfilter.xml", "appmap.xml", "theme_resource.xml", "drawable.xml", "icon_pack.xml" };
+            CheckBox[] cbControls = { checkbox0, checkbox1, checkbox2, checkbox3, checkbox4, checkbox5 };
 
             ofd.FileName = fileNames[num];
             ofd.DefaultExt = ".xml";
@@ -61,73 +66,13 @@ namespace ic_req_helper
             if (result == true)
             {
                 // Check checkbox
-                switch (num)
-                {
-                    case 0: checkbox0.IsChecked = true; break;
-                    case 1: checkbox1.IsChecked = true; break;
-                    case 2: checkbox2.IsChecked = true; break;
-                    case 3: checkbox3.IsChecked = true; break;
-                    case 4: checkbox4.IsChecked = true; break;
-                    case 5: checkbox5.IsChecked = true; break;
-                    default: break;
-                }
+                cbControls[num].IsChecked = true;
 
                 // Copy document to temp
             }
+
+            // CheckOverwriteButton()
         }
-
-        /*
-        private void AddFileToTemp(int fileNum)
-        {
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-
-            string[] filters = { "appfilter.xml", "appmap.xml", "theme_resource.xml" };
-
-            dlg.FileName = filters[fileNum];
-            dlg.DefaultExt = ".xml"; // Default file extension
-            dlg.Filter = "XML document (.xml)|*.xml"; // Filter files by extension
-
-            Nullable<bool> result = dlg.ShowDialog();
-
-            // Process open file dialog box results
-            if (result == true)
-            {
-                // Open document
-                currentPath[fileNum] = dlg.FileName;
-                tempPath[fileNum] = Path.Combine(Path.GetTempPath(), Path.GetFileNameWithoutExtension(currentPath[fileNum]) + ".xml");
-
-                // Store document to temp
-                File.Copy(currentPath[fileNum], tempPath[fileNum], true);
-
-                switch (fileNum)
-                {
-                    case 0:
-                        // Change status
-                        lblStatus1.Content = "(" + currentPath[0] + ")";
-                        lblStatus1.Foreground = new SolidColorBrush(Colors.Green);
-                        txtField1.IsEnabled = true;
-                        break;
-                    case 1:
-                        // Change status
-                        lblStatus2.Content = "(" + currentPath[1] + ")";
-                        lblStatus2.Foreground = new SolidColorBrush(Colors.Green);
-                        txtField2.IsEnabled = true;
-                        break;
-                    case 2:
-                        // Change status
-                        lblStatus3.Content = "(" + currentPath[2] + ")";
-                        lblStatus3.Foreground = new SolidColorBrush(Colors.Green);
-                        txtField3.IsEnabled = true;
-                        break;
-                    default: throw new NotImplementedException();
-                }
-
-                isFileLoaded[fileNum] = true;
-                CheckAddButton();
-            }
-
-        }
-        */
 
         private void menuReset_Click(object sender, RoutedEventArgs e)
         {
@@ -136,42 +81,34 @@ namespace ic_req_helper
 
         private void menuExit_Click(object sender, RoutedEventArgs e)
         {
-            System.Windows.Application.Current.Shutdown();
         }
 
-        private void menuLocate0_Click(object sender, RoutedEventArgs e)
+        private void menu_Click(object sender, RoutedEventArgs e)
         {
-            LocateFiles(0);
-        }
+            FrameworkElement element = e.Source as FrameworkElement;
 
-        private void menuLocate1_Click(object sender, RoutedEventArgs e)
-        {
-            LocateFiles(1);
-        }
+            if (element.Name.Equals("menuExit")) Application.Current.Shutdown();
+            else if (element.Name.Equals("menuReset")) ResetWindow();
+            else
+            {
+                int result = element.Name switch
+                {
+                    "menuLocate0" => 0,
+                    "menuLocate1" => 1,
+                    "menuLocate2" => 2,
+                    "menuLocate3" => 3,
+                    "menuLocate4" => 4,
+                    "menuLocate5" => 5,
+                    _ => throw new NotImplementedException()
+                };
 
-        private void menuLocate2_Click(object sender, RoutedEventArgs e)
-        {
-            LocateFiles(2);
-        }
-
-        private void menuLocate3_Click(object sender, RoutedEventArgs e)
-        {
-            LocateFiles(3);
-        }
-
-        private void menuLocate4_Click(object sender, RoutedEventArgs e)
-        {
-            LocateFiles(4);
-        }
-
-        private void menuLocate5_Click(object sender, RoutedEventArgs e)
-        {
-            LocateFiles(5);
+                LocateFiles(result);
+            }
         }
 
         private void btnOverwrite_Click(object sender, RoutedEventArgs e)
         {
-
+            // Todo
         }
     }
 }
