c9fb8a2177a5fc7caba76268f12cf5a635316b7e
Hadif Hatta <45189724+hdfhtt@users.noreply.github.com>
2021-06-15 14:28:42 +0800

    Added link to license

--- AboutWindow.xaml.cs
+++ AboutWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
 
 namespace ic_req_helper
 {
@@ -21,6 +23,14 @@ namespace ic_req_helper
             this.Close();
 
             Owner.IsEnabled = true;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // for .NET Core you need to add UseShellExecute = true
+            // see https://docs.microsoft.com/dotnet/api/system.diagnostics.processstartinfo.useshellexecute#property-value
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
